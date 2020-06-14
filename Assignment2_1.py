6e58e32861fc5df703d48113bc271c54839e7315
RAJ <rajvendersingh50@gmail.com>
2020-06-14 19:39:05 +0530

    I did the changes here

--- Assignment2_1.py
+++ Assignment2_1.py
@@ -9,7 +9,7 @@ def main():
     print("subtraction is ",Arithmetic.Sub(a,b))
     print("multiplication is ",Arithmetic.Mult(a,b))
     print("Division is",Arithmetic.Div(a,b))
-
+    print("I did the changes") 
 if __name__=='__main__':
     main()
 
